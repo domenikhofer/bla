e8dd546f6c017026bb5029522d2c11be52803155
Domenik Hofer <domenik.hofer@twofold.swiss>
2024-04-29 15:53:11 +0200

    Started entry editing

--- public/script.js
+++ public/script.js
@@ -1,6 +1,33 @@
-document.querySelector('.plantBottomWrapper')?.addEventListener('touchstart', function(e) {
+document.querySelector('.plantBottomWrapper')?.addEventListener('touchstart', function (e) {
     e.preventDefault();
     this.classList.toggle('clicked');
+});
+
+
+document.addEventListener('keydown', function (event) {
+    if (!event.target.parentElement.classList.contains('entry')) {
+        return;
+    }
+
+
+    let entryTitle = event.target
+    let entry = entryTitle.parentElement;
+    // save to db
+
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        let entryElement = document.querySelector('.entry.template').cloneNode(true);
+        entryElement.classList.remove('template');
+        entry.after(entryElement)
+        entryElement.querySelector('.title').focus();
+    }
+    if (event.key === 'Backspace' && entryTitle.textContent === '') {
+        event.preventDefault();
+        let previousEntry = entry.previousElementSibling;
+        previousEntry.querySelector('.title').focus();
+        setCursorToEndOfElement(previousEntry.querySelector('.title'))
+        entry.remove();
+    }
 });
 
 document.querySelectorAll('.delete-category-btn').forEach(function (btn) {
@@ -18,6 +45,16 @@ document.querySelectorAll('.delete-category-btn').forEach(function (btn) {
             confirmButtonColor: '#d33',
             cancelButtonColor: '#3085d6',
             confirmButtonText: 'Yes, delete it!',
+            showClass: {
+                popup: `
+                  swalFadeIn
+                `
+            },
+            hideClass: {
+                popup: `
+                  swalFadeOut
+                `
+            }
         }).then((result) => {
             if (result.isConfirmed) {
                 document.getElementById('form-delete-' + categoryId).submit();
@@ -25,3 +62,47 @@ document.querySelectorAll('.delete-category-btn').forEach(function (btn) {
         });
     });
 });
+
+
+function createRange(node, chars, range) {
+    if (!range) {
+        range = document.createRange()
+        range.selectNode(node);
+        range.setStart(node, 0);
+    }
+
+    if (chars.count === 0) {
+        range.setEnd(node, chars.count);
+    } else if (node && chars.count > 0) {
+        if (node.nodeType === Node.TEXT_NODE) {
+            if (node.textContent.length < chars.count) {
+                chars.count -= node.textContent.length;
+            } else {
+                range.setEnd(node, chars.count);
+                chars.count = 0;
+            }
+        } else {
+            for (var lp = 0; lp < node.childNodes.length; lp++) {
+                range = createRange(node.childNodes[lp], chars, range);
+
+                if (chars.count === 0) {
+                    break;
+                }
+            }
+        }
+    }
+
+    return range;
+};
+
+function setCursorToEndOfElement(node) {
+    var selection = window.getSelection();
+    range = createRange(node, { count: node.textContent.length });
+
+    if (range) {
+        range.collapse(false);
+        selection.removeAllRanges();
+        selection.addRange(range);
+
+    }
+};
--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -1,10 +1,149 @@
-@if ($category->children)
-@foreach ($category->children as $child)
-<a href="{{ route('category.show', ['category' => $child->id]) }}">{{ $child->name }}</a>
-@endforeach
-@else
-    @foreach ($category->entries as $entry)
-        {{ $entry->columns }}
-    @endforeach
+@include('components.head')
+<div class="entriesWrapper">
+    <h2>{{ $category->emoji }}{{ $category->name }}</h2>
 
-@endif
+    <div class="entryWrapper">
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">
+                Klima Plexiglass
+            </div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Pink Apple</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Velo Keller</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Raffi Theater Tix</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Abdeckplanen</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Kleider Org</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Wäsche</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Creme</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Kitchen utensils</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Bewerbung Garten </div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Brille Kratzer </div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Baum schneiden </div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Pflanzen züchten</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Pflanzen pflanzen</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Saisonabo bäder</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Ausflüge planen </div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Pflanzen ricardo</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Sachen Verkaufen/Vergeben</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Job</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Face yoga</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Better List App</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">DataAnnotation</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Hide Klima</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Ahv DataAnnotation</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Terasse putzen</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Sofa putzen</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Reduit ausmisten</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Keller ausmisten</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Umtopfen</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Dropbox aufräumen</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Disney Paris </div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Monstera umtopfen + Bambusstab</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Produkte aufbrauchen</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">ÖV Abo überprüfen </div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Luftbefeuchter Keller</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Pflanzen giessen wöchentlich</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Balkon pflanze verschieben</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Pflanzen Balkon</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Stretching routine </div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Meditation?</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Folie Rückwand Küche</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Gampel Tickets </div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Raiffeisen wechseln</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Checkup / Blutbild</div>
+        </div>
+        <div class="entry">
+            <div class="title" contenteditable="true" spellcheck="false">Badi Abo ZH</div>
+        </div>
+        <div class="entry template">
+            <div class="title" contenteditable="true" spellcheck="false"></div>
+        </div>
+    </div>
+</div>
+
+@include('components.foot')
