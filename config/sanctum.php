89da645f034e41c8da6337ff3dcc26c10dc64a7a
Domenik Hofer <domenik.hofer@twofold.swiss>
2024-08-05 17:22:43 +0200

    started authentication

--- config/sanctum.php
+++ config/sanctum.php
@@ -80,4 +80,13 @@ return [
         'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
     ],
 
+       /*
+    |--------------------------------------------------------------------------
+    | Sanctum Route prefix
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'prefix' => 'api',
+
 ];
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,34 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\EntryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only('email', 'password');
+
+    if (Auth::attempt($credentials)) {
+        // User authenticated successfully
+        return response()->json([
+            'message' => 'Login successful!',
+        ]);
+    } else {
+        return response()->json([
+            'message' => 'Invalid login credentials!',
+        ], 401);
+    }
+});
+
+Route::post('/logout', function () {
+    auth()->logout(); // For session-based authentication
+
+    return response()->json([
+        'message' => 'Successfully logged out!',
+    ]);
+});
 
 Route::get('/entry/search', [EntryController::class, 'search']);
 Route::post('entry/store-media', [EntryController::class, 'storeMedia'])->name('entry.storeMedia');
